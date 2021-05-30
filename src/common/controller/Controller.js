1f0d7494a9e5c46492f9eeb672e852f2c1a5b184
sai darshan <saidarshan27@gmail.com>
2021-05-31 01:35:14 +0530

    move header to separate screens, successfully fetch user full posts

--- src/common/controller/Controller.js
+++ src/common/controller/Controller.js
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import {Route,Switch,useHistory, useLocation} from "react-router-dom";
+import {Route,Switch} from "react-router-dom";
 
 
 import Login from "../../screens/login/Login";
--- src/screens/home/post/Post.js
+++ src/screens/home/post/Post.js
@@ -1,11 +1,41 @@
 import {useState} from "react";
+import {Card,CardHeader,CardMedia,CardContent,CardActions,Avatar,IconButton,Typography, makeStyles} from "@material-ui/core";
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
+const useStyles = makeStyles({
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    }
+})
 
 function Post(props){
+    console.log(props);
+    const classes = useStyles();
     return(
-        <span>
-            {props.caption}
-        </span>
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar className={classes.avatar} src={props.post.profilepicURL}>  
+          </Avatar>
+        }
+        title={props.post.username}
+      />
+      <CardMedia
+        className={classes.media}
+        image={props.post.media_url}
+       />
+      <CardContent>
+        <Typography variant="body2" color="textPrimary" component="p">
+          {props.caption}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton>
+          <FavoriteIcon />
+        </IconButton>
+       </CardActions>
+    </Card>
     );
 }
 
