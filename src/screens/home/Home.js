920f49244712cd94d3936fab8bb56a4e6ff2f87b
sai darshan <saidarshan27@gmail.com>
2021-05-30 15:03:33 +0530

    fetch media ids from api and fill corresponding objects with image and media type

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,6 +1,48 @@
+import {useEffect, useState} from "react";
 
 function Home(){
-    return("HOME");
+    // state
+    const [userData,setUserData] = useState(null);
+
+    const baseURL = 'https://graph.instagram.com';
+    const accessToken = sessionStorage.getItem("access-token");
+
+    const relativeResourceURL = `/me/media?fields=id,caption&access_token=${accessToken}`;
+    const absoluteURL = `${baseURL}${relativeResourceURL}`;
+
+    useEffect(()=>{
+        if(!userData){
+            (async()=>{
+                let response = await fetch(absoluteURL);
+                let responseJSON = await response.json();
+        
+                let userPosts = responseJSON.data;
+
+                userPosts.forEach(async(userPost,index)=>{
+                    let userSpecificPostURL = `/${userPost.id}?fields=id,media_type,media_url,username,timestamp&access_token=${accessToken}`;
+                    let URL = `${baseURL}${userSpecificPostURL}`;
+
+                    let response = await fetch(URL);
+                    let responseJSON = await response.json();
+
+                    userPosts[index] = {...userPost,...responseJSON};
+                });
+                setUserData(userPosts);           
+            })();
+        }
+    },[]);
+
+    return (
+        userData && 
+        userData.map((post)=>{
+            return(
+            <span key={post.id}>
+                {post.caption}
+            </span>
+            )
+        })
+    )
+    
 }
 
 export default Home;
